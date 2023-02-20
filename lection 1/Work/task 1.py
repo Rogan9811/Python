f0056c6984e614846ada576a6d8bc810b9fb7439
Danil Blykher <mrx45998@gmail.com>
2023-02-21 00:24:06 +0300

    Add HW1

---
+++ lection 1/Work/task 1.py
@@ -0,0 +1,4 @@
+f_num = int(input('ENter 1 num: '))
+s_numb = int(input('Enter 2 num: '))
+
+print(-(-s_numb // f_num))
---
+++ lection 1/Work/task 2.py
@@ -0,0 +1,5 @@
+f_class = int(input('1 школьник'))
+s_class = int(input('2 школьник'))
+t_class = int(input('3 школьник'))
+
+print(-((-f_class - s_class - t_class)//2))
