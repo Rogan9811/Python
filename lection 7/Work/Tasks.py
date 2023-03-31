9e59a7de195d15ebe321bc55dbd772eca70b40c1
Danil Blykher <mrx45998@gmail.com>
2023-03-31 19:40:05 +0300

    New HW

---
+++ lection 7/Work/Tasks.py
@@ -0,0 +1,33 @@
+data = [1, 2, 3, 4, 5, 6, 7, 9, 10]
+res = list()
+
+# # for i in data:
+# #     if i % 2 == 0:
+# #         res.append((i, i**2))
+
+# # print(res)
+
+# def select(f, col):
+#     return [f(x) for x in col]
+
+# def where(f, col):
+#     return [x for x in col if f(x)]
+
+# res = map(int, data)
+# res = filter(lambda x: x % 2 == 0, res)
+# res = list(map(lambda x: (x, x ** 2), res))
+# print(res)
+
+# list_1 = [i for i in range(1, 20)]
+# print(list_1)
+
+# list_1 = list(map(lambda x: x + 10, list_1))
+# print(list_1)
+
+# data = list(map(int, data.split()))
+# print(data)
+
+
+# data = [15, 64, 234, 345, 42, 131]
+# res = list(filter(lambda x: x % 10 == 5, data)) 
+# print(res)
